f3543d8377f80bb7514704a854defd02642df8e5
mahfuj02 <ahmed.mahfuj02@gmail.com>
2025-03-05 11:42:42 -0600

    Configure ActiveAdmin to handle Customer image uploads

--- app/admin/customers.rb
+++ app/admin/customers.rb
@@ -1,18 +1,27 @@
+# Edit app/admin/customers.rb
 ActiveAdmin.register Customer do
-
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :full_name, :phone_number, :email_address, :notes
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:full_name, :phone_number, :email_address, :notes]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
+  permit_params :full_name, :phone_number, :email_address, :notes, :image
   
+  form do |f|
+    f.inputs do
+      f.input :full_name
+      f.input :phone_number
+      f.input :email_address
+      f.input :notes
+      f.input :image, as: :file
+    end
+    f.actions
+  end
+  
+  show do
+    attributes_table do
+      row :full_name
+      row :phone_number
+      row :email_address
+      row :notes
+      row :image do |customer|
+        image_tag customer.image if customer.image.attached?
+      end
+    end
+  end
 end
