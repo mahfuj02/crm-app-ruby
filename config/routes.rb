8a6afcdd993ece0e7822160629d5746e95ecf5e4
mahfuj02 <ahmed.mahfuj02@gmail.com>
2025-03-05 12:43:38 -0600

    Final adjustments and fixes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
-# Edit config/routes.rb
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   
+  # Define your routes for the customers controller
   root 'customers#index'
-  get 'customers/alphabetized'
-  get 'customers/missing_email'
+  
+  # Add this line to define standard RESTful routes for customers
+  resources :customers, only: [:index]
+  
+  # Add the custom routes for missing_email and alphabetized
+  get 'customers/alphabetized', to: 'customers#alphabetized'
+  get 'customers/missing_email', to: 'customers#missing_email'
 end
